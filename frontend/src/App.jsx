30e0731589fa29d4d2017092f1ff139dd588daa0
SyazwanYacobUiTMJasin <muhdsyazwan5703@gmail.com>
2024-12-12 08:32:18 +0800

    added logo for corporate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Scoreboard from "./components/Scoreboard";
 import "./App.css";
+import logoisec from './assets/logoisec.png';
+import logosulam from './assets/logosulam.png';
+import logouitmwhite from './assets/logouitmwhite.png';
 
 function App() {
     const handleSave = () => {
@@ -25,12 +28,18 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <h1>Leaderboard Scoreboard</h1>
+                <div className="logos">
+                    <img src={logoisec} alt="logoisec" />
+                    <img src={logosulam} alt="logosulam" />
+                    <img src={logouitmwhite} alt="logouitmwhite" /> 
+                </div>
                 <span className="buttons">
                     <button onClick={handleSave} className="save-button">Save</button>
                     {/* <button onClick={handleReset} className="reset-button">Reset</button> */}
                 </span>
             </header>
             <Scoreboard />
+            <p className="footer">UiTM JASIN x KOLEJ YAYASAN SAAD</p>
         </div>
     );
 }
